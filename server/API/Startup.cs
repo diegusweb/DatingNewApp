85dc47229e1a3f8c691c96e89256c4ed0eaf2bfe
Diego Rueda <diego.rueda2@gmail.com>
2020-11-10 17:22:28 -0400

    Adding a token service

--- server/API/Startup.cs
+++ server/API/Startup.cs
@@ -12,6 +12,8 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using API.Interfaces;
+using API.Services;
 
 namespace API
 {
@@ -27,6 +29,8 @@ namespace API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddScoped<ITokenService, TokenServices>();
+            
             services.AddDbContext<DataContext>(options =>
             {
                 options.UseSqlServer(_config.GetConnectionString("DevConnection"));
