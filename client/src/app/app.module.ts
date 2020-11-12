bdb8a6617040f6e31f872734a39b2758bdc538f0
Diego Rueda <diego.rueda2@gmail.com>
2020-11-12 13:06:34 -0400

    Angular template forms

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,11 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NavComponent } from './nav/nav.component';
+
 
 @NgModule({
   declarations: [
@@ -16,7 +18,8 @@ import { NavComponent } from './nav/nav.component';
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
