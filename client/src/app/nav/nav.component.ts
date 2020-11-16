1bf6d4da900ff0d0d550c57c331488b302c67fec
Diego Rueda <diego.rueda2@gmail.com>
2020-11-16 13:52:46 -0400

    error hanlder- Handling not found

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -22,9 +22,6 @@ export class NavComponent implements OnInit {
   login(){
     this.accountServices.login(this.model).subscribe(response => {
       this.router.navigateByUrl('/members');
-
-    }, error => {
-      this.toastr.error(error.error);
     })
   }
 
