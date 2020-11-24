bafd5d24c76ee7da4978bf05ebb005d640058858
Diego Rueda <diego.rueda2@gmail.com>
2020-11-24 16:02:36 -0400

    seccion 9 - Creating the member interface

---
+++ client/src/app/_modal/member.ts
@@ -0,0 +1,17 @@
+import { Photo } from "./photo";
+
+export interface Member {
+    UserName: string;
+    Gender: string;
+    DateOfBirth: string;
+    KnownAs: string;
+    Created: string;
+    LastActive: string;
+    Introduction: string;
+    LookingFor: string;
+    Interests: string;
+    City: string;
+    Country: string;
+    Photos: Photo[];
+  }
+  
